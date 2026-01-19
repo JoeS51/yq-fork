e88946e2d71befe63019b1366b2da655c4d6b895
Johannes Sluis <joesluis51@gmail.com>
2026-01-19 12:22:02 -0800

    Fix unit test

--- pkg/yqlib/lib_test.go
+++ pkg/yqlib/lib_test.go
@@ -102,7 +102,12 @@ func TestParseSnippet(t *testing.T) {
 			if err == nil {
 				t.Errorf("Expected error '%v' but it worked!", tt.expectedError)
 			} else {
-				test.AssertResultComplexWithContext(t, tt.expectedError, err.Error(), tt.snippet)
+				test.AssertResultWithContext(
+					t,
+					true,
+					strings.Contains(err.Error(), tt.expectedError),
+					fmt.Sprintf("Expected [%v] to contain [%v]", err.Error(), tt.expectedError),
+				)
 			}
 			continue
 		}
