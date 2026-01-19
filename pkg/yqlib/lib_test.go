02aca2b2c3658803ea31112675f51cb3c9f31efa
Johannes Sluis <joesluis51@gmail.com>
2026-01-19 11:51:42 -0800

    Revert commit

--- pkg/yqlib/lib_test.go
+++ pkg/yqlib/lib_test.go
@@ -24,7 +24,6 @@ type parseSnippetScenario struct {
 var parseSnippetScenarios = []parseSnippetScenario{
 	{
 		snippet:       ":",
-		// Error message is produced by go-yaml; match stable substring to avoid brittle tests.
 		expectedError: "did not find expected key",
 	},
 	{
